ce36d0e7bca69628baa3da73986d28ab84ca807f
kaisertuan98 <37129933+kaisertuan98@users.noreply.github.com>
2018-04-21 09:20:59 +0700

    Add files via upload

---
+++ Java Input-Output Exercises [18 exercises with solution]/bai1.java
@@ -0,0 +1,15 @@
+package baitap;
+
+import java.io.File;
+import java.util.Date;
+
+public class bai1 {
+     public static void main(String a[])
+     {
+        File file = new File("/home/students/");
+        String[] fileList = file.list();
+        for(String name:fileList){
+            System.out.println(name);
+        }
+    }
+}
